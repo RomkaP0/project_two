b164aec7800d1852f728058f33120a2dea996544
romkap0 <roman.kradyk@gmail.com>
2022-12-28 18:09:49 +0700

    Firebase add

---
+++ src/components/AddDist.tsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import {addDoc, collection} from "firebase/firestore";
+import {db} from "../lib/init-firebase";
+
+
+export default function AddDist(){
+
+    const [Distance, setDist]= useState('')
+
+    function handleSubmit(e: { preventDefault: () => void; }){
+        e.preventDefault()
+        let numDist=Number(Distance)
+        if (Distance ===''||isNaN(numDist)){
+            return
+        }
+
+        // const  distanceCollRef = collection(db, 'UserDistance')
+        // addDoc(distanceCollRef, {Distance:numDist}).then(response=>{
+        //     console.log(response.id)
+        // }).catch(error=> {
+        //     console.log(error.message)
+        // })
+        //
+        // alert(Distance)
+    }
+
+    return(
+        <div>
+            <h4>
+                AddDistance
+            </h4>
+            <form className='FormDistance ' onSubmit={handleSubmit}>
+                <label htmlFor='name'>input dist</label>
+                <input className='inputdist border - 5px margin 6px'
+                    id='Model'
+                    type='text'
+                    value={Distance}
+                    onChange={e => setDist(e.target.value)}
+                />
+                <button type='submit'>Change distance</button>
+            </form>
+        </div>
+    )
+}
--- src/components/ChangeDist.tsx
+++ src/components/ChangeDist.tsx
@@ -1,11 +1,10 @@
-import {useEffect, useState} from "react";
+import React, {useState} from 'react';
 import {addDoc, collection} from "firebase/firestore";
 import {db} from "../lib/init-firebase";
 
-
-export default function ChangeDist(){
-
+function ChangeDist() {
     const [Distance, setDist]= useState('')
+    const [id, setId] = useState('')
 
     function handleSubmit(e: { preventDefault: () => void; }){
         e.preventDefault()
@@ -30,15 +29,24 @@ export default function ChangeDist(){
                 ChangeDistance
             </h4>
             <form className='FormDistance border - 5px' onSubmit={handleSubmit}>
-                <label htmlFor='name'>Car Name</label>
+                <label htmlFor='id'>Distance ID</label>
+                <input
+                    id='id'
+                    type='text'
+                    value={id}
+                    onChange={e => setId(e.target.value)}
+                />
+                <label htmlFor='Distance'>Value</label>
                 <input
                     id='Model'
                     type='text'
                     value={Distance}
                     onChange={e => setDist(e.target.value)}
                 />
-                <button type='submit'>Change distance</button>
+                <button type='submit'>Update distance</button>
             </form>
         </div>
     )
 }
+
+export default ChangeDist;
