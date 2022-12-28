04c9a48ac43ba717a1122c6562da68e0ae233eba
romkap0 <roman.kradyk@gmail.com>
2022-12-29 05:02:04 +0700

    Firebase add

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import ListCars from "./components/ListCars";
 import AddDist from "./components/AddDist";
 import WatchDist from "./components/WatchDist";
 import ChangeDist from "./components/ChangeDist";
+import MapContainer from './components/MapContainer';
+import Location from "./components/Location";
+
 
 function App() {
     return (
-        <div className="container mx-auto max-w-5xl pt-5">
-            <h1>Hello</h1>
-            <WatchDist/>
-            <ChangeDist/>
-            <AddDist/>
-            <ListCars/>
+        <div className="container">
+            <MapContainer/>
+            <Location/>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />
-);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
